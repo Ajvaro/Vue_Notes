fd0d56332e8956326b13257fa489974c907b1929
Nikola Dimitrijevic <nikola.dmtrj@gmail.com>
2018-03-30 20:48:42 +0200

    styling done

---
+++ src/components/Note.vue
@@ -0,0 +1,89 @@
+<template>
+
+  <div class="note">
+    <a href="#" class="note__delete">
+      <span class="note__delete-icon">&times;</span>
+    </a>
+    <div class="note__content">
+      <a href="#" class="note__title">Note one</a>
+      <p class="note__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi facere fugit inventore magni.</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .note {
+    display: flex;
+    color: inherit;
+    text-decoration: none;
+    border-bottom: 2px solid #eee;
+    opacity: .6;
+    flex-direction: row;
+    max-width: 100%;
+
+    &:hover {
+      opacity: 1;
+
+      .note__delete {
+        margin-left: 0;
+      }
+    }
+
+    &__content {
+      padding: 30px;
+      flex: 2;
+      overflow: hidden;
+    }
+
+    &__title {
+      display: block;
+      font-size: 1em;
+      margin: 0;
+      font-weight: 500;
+      text-decoration: none;
+      color: inherit;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    &__body {
+      font-size: .9em;
+      margin-bottom: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    &__delete {
+      position: relative;
+      margin-left: -50px;
+      width: 50px;
+      display: block;
+      background: #cc4466;
+      min-height: 100%;
+      color: #fff;
+      transition: 100ms margin-left ease-in-out;
+    }
+
+    &__delete-icon {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 1.5em;
+      font-weight: 600;
+    }
+  }
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="sidebar">
+    <note></note>
+    <note></note>
+    <note></note>
+    <!--<div class="sidebar__content">-->
+      <!--Boo, no notes... Write something-->
+    <!--</div>-->
+  </div>
+
+</template>
+
+<script>
+  import Note from './Note'
+
+  export default {
+    components: {
+      Note
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .sidebar {
+    background-color: #f9f9f9;
+    flex: 1;
+    height: 100%;
+    max-width: 25%;
+    border-right: 2px solid #eee;
+    overflow-y: auto;
+
+    &__content {
+      padding: 30px;
+
+      a {
+        text-decoration: none;
+        color:inherit;
+      }
+    }
+  }
+
+</style>
